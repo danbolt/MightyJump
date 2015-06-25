6cf15f2734a2c72239ea92f4311d2901e480e646
Daniel Savage <dansvg@gmail.com>
2015-06-25 13:51:28 -0700

    Add player physics body

--- src/script/gameplay.js
+++ src/script/gameplay.js
@@ -1,5 +1,7 @@
 var GameplayState =
 {
+	player: null,
+
 	preload: function()
 	{
 		//
@@ -7,11 +9,24 @@ var GameplayState =
 
 	create: function()
 	{
+		// Initialize the physics system
+		game.physics.startSystem(Phaser.Physics.ARCADE);
+
+		// Instantiate the player
+		this.player = game.add.sprite(0, 0, null);
+		game.physics.enable(this.player);
+		this.player.body.setSize(16, 16);
+	},
+
+	update: function()
+	{
 		//
 	},
 
 	render: function()
 	{
 		game.debug.text(window.mobileAndTabletCheck() ? "mobile!" : "desktop", 16, 16, 'white');
+
+		game.debug.body(this.player, 'blue');
 	}
 };
