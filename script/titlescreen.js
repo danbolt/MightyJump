26578e7cd21981e78d770174b0cbe982ba3e56f9
Daniel Savage <dansvg@gmail.com>
2015-07-04 16:09:16 -0700

    Touch buttons stay on every state now

--- script/titlescreen.js
+++ script/titlescreen.js
@@ -8,16 +8,38 @@ var TitleScreen =
 
 		game.add.text(80, 240 / 2, 'title screen', { font: '8px Conv_Gamegirl', fill: 'white', align: 'center' });
 
-		game.add.text(48, 240 / 2 + 32, 'press space to start', { font: '8px Conv_Gamegirl', fill: 'white', align: 'center' });
+		game.add.text(48, 240 / 2 + 32, 'press A or X to start', { font: '8px Conv_Gamegirl', fill: 'white', align: 'center' });
+
+				// Initialize the touchscreen buttons
+		var leftButton = game.add.button(0, 240, 'touchbuttons', undefined, this, 0, 0, 1, 0);
+		leftButton.onInputDown.add(function() { LeftButtonDown = true; }, this);
+		leftButton.onInputUp.add(function() { LeftButtonDown = false; }, this);
+		leftButton.onInputOut.add(function() { LeftButtonDown = false; }, this);
+		leftButton.fixedToCamera = true;
+		var rightButton = game.add.button(64, 240, 'touchbuttons', function() {}, this, 2, 2, 3, 2);
+		rightButton.onInputDown.add(function() { RightButtonDown = true; }, this);
+		rightButton.onInputUp.add(function() { RightButtonDown = false; }, this);
+		rightButton.onInputOut.add(function() { RightButtonDown = false; }, this);
+		rightButton.fixedToCamera = true;
+		var bButton = game.add.button(128, 240, 'touchbuttons', function() {}, this, 4, 4, 5, 4);
+		bButton.onInputDown.add(function() { BButtonDown = true; }, this);
+		bButton.onInputUp.add(function() { BButtonDown = false; }, this);
+		bButton.onInputOut.add(function() { BButtonDown = false; }, this);
+		bButton.fixedToCamera = true;
+		var aButton = game.add.button(192, 240, 'touchbuttons', function() {}, this, 6, 6, 7, 6);
+		aButton.onInputDown.add(function() { AButtonDown = true; }, this);
+		aButton.onInputUp.add(function() { AButtonDown = false; }, this);
+		aButton.onInputOut.add(function() { AButtonDown = false; }, this);
+		aButton.fixedToCamera = true;
 	},
 
 	update: function()
 	{
-		if (game.input.keyboard.isDown(Phaser.Keyboard.SPACEBAR) && this.spaceDown == false)
+		if ((game.input.keyboard.isDown(Phaser.Keyboard.X) || AButtonDown) && this.spaceDown == false)
 		{
 			this.spaceDown = true;
 		}
-		else if (!(game.input.keyboard.isDown(Phaser.Keyboard.SPACEBAR)) && this.spaceDown == true)
+		else if (!(game.input.keyboard.isDown(Phaser.Keyboard.X) || AButtonDown) && this.spaceDown == true)
 		{
 			this.spaceDown = false;
 
